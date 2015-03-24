c0326d0014a59d891861fb14f3dfffa633d1e2f0
briiju <bieszkeh@gmail.com>
2015-03-23 20:02:02 -0400

    Added project file

---
+++ ECGFilter.ino
@@ -0,0 +1,62 @@
+
+
+/*
+  This program is meant to take information from the analog input A0
+  and print it out to a text file called "CardioCheckData.txt"
+  Sample rate: 200Hz (project requires at least 160Hz)
+  
+ */
+
+#include <SPI.h>
+#include <SD.h>
+#include <ECGFilters.h>
+
+int i;
+int sensorValue;
+boolean SDInitSuccess = true;
+File myFile;
+qrs_filter filter = qrs_filter();
+
+void setup()
+{
+  // Open serial communications and wait for port to open: use for debug purposes
+  //Serial.begin(115200);
+  
+  pinMode(10, OUTPUT);
+  digitalWrite(10, HIGH);
+  //Serial.println("Initializing SD Card");
+  if (!SD.begin(10)) {
+    SDInitSuccess = false;
+    //Serial.println("SD card Not initialized");
+    return;
+  }
+  //Serial.println("SD initialization success");
+  
+  //creating and opening file
+  myFile = SD.open("test.txt", FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+    //Serial.print("File created successfully. Writing analog info");
+    for (i = 0; i <= 1000; i++)
+    {
+      sensorValue = analogRead(A0);
+      return_value result = filter.step(sensorValue);
+      myFile.println(millis());
+      //myFile.println(analogRead(0));
+    }
+    // close the file:
+    myFile.close();
+    //Serial.println("done writing to file");
+  } else {
+    // if the file didn't open, print an error:
+    //Serial.println("error opening CardioCheckData.txt");
+  }
+}
+
+void loop()
+{
+  // nothing happens after setup
+}
+
+
